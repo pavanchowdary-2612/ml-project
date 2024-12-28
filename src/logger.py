3fe15982083aface2ce5894521d3480656c63a45
pavan.k <pavanchowdary2612@gmail.com>
2024-12-29 00:52:00 +0530

    addng logger file-2

--- src/logger.py
+++ src/logger.py
@@ -13,3 +13,6 @@ logging.basicconfig(
     format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
 )
+
+if __name__ =="__main__":
+    logging.info("Logging has started")
